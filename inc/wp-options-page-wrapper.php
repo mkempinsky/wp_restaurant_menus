e5c368281fec4b1b453abb5fe39009b2caa5d69f
Michele Kempinsky <mkempinsky@icloud.com>
2017-10-29 10:39:13 -0700

    first commit of plugin

---
+++ inc/wp-options-page-wrapper.php
@@ -0,0 +1,107 @@
+
+
+
+<div class="wrap">
+
+	<div id="icon-options-general" class="icon32"></div>
+	<h1>Restaurant Menus</h1>
+
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-2">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+				<div class="meta-box-sortables ui-sortable">
+
+					<div class="postbox">
+
+						<div class="handlediv" title="Click to toggle"><br></div>
+						<!-- Toggle -->
+
+						<h2 class="hndle"><span>Let's Get Started</span>
+						</h2>
+
+						<div class="inside">
+							<form method="post" action="">
+							<table class="form-table">
+								<tr>
+									<td class="row-title">
+										<label for="wptreehouse_username">Restaurant Name</label>
+									</td>
+									<td><input type="text" value=".regular-text" class="regular-text" /><br></td>
+								</tr>
+							</table>
+							<p>
+								<input class="button-primary" type="submit" name="wptreehouse_username_sumbit" value="Save" />
+							</p>
+							</form>
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+
+					<div class="postbox">
+
+						<div class="handlediv" title="Click to toggle"><br></div>
+						<!-- Toggle -->
+
+						<h2 class="hndle"><span>Most Recent Badges</span>
+						</h2>
+
+						<div class="inside">
+
+						</div>
+						<!-- .inside -->
+
+						<ul class="wptreehouse-badges">
+
+						</ul>
+
+					</div>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
+
+			</div>
+			<!-- post-body-content -->
+
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
+
+				<div class="meta-box-sortables">
+
+					<div class="postbox">
+						<h3><span>Mike the Frog's Profile</span></h3>
+						<div class="inside">
+							
+
+
+						</div> <!-- .inside -->
+
+						<div class="handlediv" title="Click to toggle"><br></div>
+						<!-- Toggle -->
+
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables -->
+
+			</div>
+			<!-- #postbox-container-1 .postbox-container -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <!-- .wrap -->
---
+++ wptreehouse-badges.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Restaurant Menus Plugin
+Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin
+Description: Provides backend access to update menu items
+Version: 1.0
+Author: Michele Kempinsky
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+*/
+
+
+/*
+	Assign Global Variables
+*/
+
+	$plugin_url = WP_PLUGIN_URL . '/wp_restaurant_menus';
+
+/*
+Add a link to our plugin in the admin menu
+under 'settings' > Treehouse Badges
+*/
+
+function wp_restaurant_menus(){
+
+	/*use the add_options_page function
+		add_options_page accepts the following peramiters:
+			$page_title
+			$menu_title
+			$capability (level of user that is allowed to access plugin page)
+			$menu-slug
+			$function (the function we want to call, which tells us what we want to appear on options page)
+	 */
+
+
+	add_menu_page()(
+		'Restaurant Menus',
+		'Restaurant Menus',
+		'manage_options',
+		'restaurant-menus',
+		'restaurant_menu_option_page'
+		);
+}
+
+
+add_action( 'admin_menu', 'wp_restaurant_menus' );
+
+function restaurant_menu_option_page(){
+
+	if(!current_user_can( 'manage_options' )){
+		wp_die('You do not have sufficient permissions to access this page.' );
+	}
+
+	global $plugin_url;
+	
+	// require options page wrapper for wordpress admin style page
+	require('inc/options-page-wrapper.php');
+}
+
+function wptreehouse_badges_styles(){
+	wp_enqueue_style( 'wptreehouse_badges_styles', plugins_url('wp_treehouse_badges/wptreehouse-badges.css') );
+}
+
+add_action( 'admin_head', 'wptreehouse_badges_styles');
+
+
+?>		
